330f4002e5e53e3dc7f63cafd4a7f23e2c1d3e1a
chao <82521372+chaoxiaozhu@users.noreply.github.com>
2022-04-16 19:51:26 +0800

    Update x86_64.sh

--- scripts/x86_64.sh
+++ scripts/x86_64.sh
@@ -11,9 +11,6 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 # 修改主机名字，把OpenWrt修改你喜欢的就行（不能纯数字或者使用中文）
 #sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' package/lean/default-settings/files/zzz-default-settings
 
-# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
-
 # 开启 WiFi
 #sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
