da6df89cf55eef790d88c0bf8795b2710880b5d5
chao <82521372+chaoxiaozhu@users.noreply.github.com>
2023-01-30 21:27:09 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -6,8 +6,8 @@
 # 添加自定义组件
 git clone https://github.com/kenzok8/small package/small
 git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
-#git clone https://github.com/liuran001/openwrt-packages package/liuran001
-#git clone https://github.com/xiaorouji/openwrt-passwall package/xiaorouji
+git clone https://github.com/liuran001/openwrt-packages package/liuran001
+git clone https://github.com/xiaorouji/openwrt-passwall package/xiaorouji
 #git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy
 
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
