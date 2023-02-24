5177b7e51504ba607b6541bd9a10ffc577224cb5
chao <82521372+chaoxiaozhu@users.noreply.github.com>
2023-02-24 17:32:38 +0800

    Create lienol.sh

---
+++ scripts/lienol.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+
+# 添加组件
+git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
+git clone https://github.com/liuran001/openwrt-packages package/liuran001
+git clone https://github.com/xiaorouji/openwrt-passwall package/xiaorouji
+git clone https://github.com/kenzok8/small package/smal
+
+# 空密码
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/default-settings/files/zzz-default-settings
+
+# 修改openwrt登陆地址,把下面的192.168.10.1修改成你想要的就可以了，其它不要动
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+# 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
+sed -i 's/"TTYD 终端"/"TTYD"/g' feeds/luci/transplant/luci-app-ttyd/po/zh-cn/terminal.po
+sed -i 's/"带宽监控"/"监控"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/other/lean/luci-app-turboacc/po/zh-cn/turboacc.po
