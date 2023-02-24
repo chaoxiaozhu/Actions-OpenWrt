d7cb2726d66922121ff15febd504a49dbb979199
chao <82521372+chaoxiaozhu@users.noreply.github.com>
2023-02-25 01:10:44 +0800

    Update lienol.sh

--- scripts/lienol.sh
+++ scripts/lienol.sh
@@ -7,7 +7,7 @@ git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
 git clone https://github.com/liuran001/openwrt-packages package/liuran001
 git clone https://github.com/xiaorouji/openwrt-passwall package/xiaorouji
 git clone https://github.com/kenzok8/small package/smal
-git clone https://github.com/chaoxiaozhu/diy package/chao
+git clone https://github.com/chaoxiaozhu/diy package/diy
 
 # 删除文件
 rm -rf feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
@@ -15,13 +15,15 @@ rm -rf feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-
 rm -rf feeds/lienol/luci-app-fileassistant/htdocs/luci-static/resources/fileassistant/fb.js
 rm -rf feeds/lienol/luci-app-fileassistant/luasrc/controller/fileassistant.lua
 rm -rf feeds/luci/applications/luci-app-advanced-reboot/po/zh_Hans/advanced-reboot.po
+rm -rf feeds/luci/collections/luci/Makefile
 
 # 复制文件
-mv package/chao/luci-app-ttyd.json feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d
-mv package/chao/luci-app-nlbwmon.json feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d
-mv package/chao/fb.js feeds/lienol/luci-app-fileassistant/htdocs/luci-static/resources/fileassistant
-mv package/chao/fileassistant.lua feeds/lienol/luci-app-fileassistant/luasrc/controller
-mv package/chao/advanced-reboot.po feeds/luci/applications/luci-app-advanced-reboot/po/zh_Hans
+mv package/diy/luci-app-ttyd.json feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d
+mv package/diy/luci-app-nlbwmon.json feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d
+mv package/diy/fb.js feeds/lienol/luci-app-fileassistant/htdocs/luci-static/resources/fileassistant
+mv package/diy/fileassistant.lua feeds/lienol/luci-app-fileassistant/luasrc/controller
+mv package/diy/advanced-reboot.po feeds/luci/applications/luci-app-advanced-reboot/po/zh_Hans
+mv package/diy/Makefile feeds/luci/collections/luci
 
 # 修改openwrt登陆地址,把下面的192.168.10.1修改成你想要的就可以了，其它不要动
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
