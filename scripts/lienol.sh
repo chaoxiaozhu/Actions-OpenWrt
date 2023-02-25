7cd27737492a60666d98e13237d90bfe585905be
chao <82521372+chaoxiaozhu@users.noreply.github.com>
2023-02-25 10:12:50 +0800

    Update lienol.sh

--- scripts/lienol.sh
+++ scripts/lienol.sh
@@ -28,5 +28,5 @@ mv package/diy/Makefile feeds/luci/collections/luci
 # 修改openwrt登陆地址,把下面的192.168.10.1修改成你想要的就可以了，其它不要动
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
+# 修改插件名字
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/other/lean/luci-app-turboacc/po/zh-cn/turboacc.po
